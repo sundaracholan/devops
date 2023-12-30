4159930383849798823d3a0480ca894447323c71
Ubuntu <ubuntu@ip-172-31-11-254.ap-south-1.compute.internal>
2023-12-30 12:08:31 +0000

    Added new scripts

---
+++ add.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+string1=10
+string1=20
+echo "The value of string1 is $string1 "
+echo "The valeu of string1 is $string1 "
---
+++ name.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+name="john"
+echo "Hello, $name!"
+
